664a964ff7b32127e2c649864745c95053a17635
henriquesv <henriquedasilva467@gmail.com>
2021-08-04 21:14:34 -0300

    'teste2'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Reset } from 'styled-reset';
-
-import Home from './pages/Home';
 import theme from './theme';
+import Home from './pages/Home';
+import {Reset} from 'styled-reset';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Reset />
-      <Home />
-    </ThemeProvider>
-  );
+
+function App(){
+
+  return <ThemeProvider theme={theme}>
+<Reset />
+<Home />
+  </ThemeProvider>
 }
 
 export default App;
